e6b2f8375e472cc29fadab384e2d34c29babdbeb
internetbuilder <45112383+internetbuilder@users.noreply.github.com>
2022-10-02 11:00:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# gamz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-kufvwe)
---
+++ main.js
@@ -0,0 +1,25 @@
+import './style.css';
+import javascriptLogo from './javascript.svg';
+import { setupCounter } from './counter.js';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="#" target="_blank">
+      <img src="/gamz.svg" class="logo" alt="Gamz!" />
+    </a>
+    <!-- 
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    -->
+    <h1>Gamz!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+    
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector('#counter'));
